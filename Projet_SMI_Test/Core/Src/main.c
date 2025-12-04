bbdc51b03cbc0f24fffe3d17ac9e2fe6207d9f9a
Alexis <alexis.cote.3@ulaval.ca>
2025-12-04 12:54:11 -0500

    init

---
+++ Projet_SMI_Test/Core/Src/main.c
@@ -0,0 +1,641 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim4;
+
+/* Definitions for blinkGreenTask */
+osThreadId_t blinkGreenTaskHandle;
+const osThreadAttr_t blinkGreenTask_attributes = {
+  .name = "blinkGreenTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for blinkRedTask */
+osThreadId_t blinkRedTaskHandle;
+const osThreadAttr_t blinkRedTask_attributes = {
+  .name = "blinkRedTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for setPWM */
+osThreadId_t setPWMHandle;
+const osThreadAttr_t setPWM_attributes = {
+  .name = "setPWM",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for readHumidity */
+osThreadId_t readHumidityHandle;
+const osThreadAttr_t readHumidity_attributes = {
+  .name = "readHumidity",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for readTouchLCD */
+osThreadId_t readTouchLCDHandle;
+const osThreadAttr_t readTouchLCD_attributes = {
+  .name = "readTouchLCD",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for setLCDState */
+osThreadId_t setLCDStateHandle;
+const osThreadAttr_t setLCDState_attributes = {
+  .name = "setLCDState",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for sleepTimer */
+osTimerId_t sleepTimerHandle;
+const osTimerAttr_t sleepTimer_attributes = {
+  .name = "sleepTimer"
+};
+/* Definitions for BlinkTurnMutex */
+osMutexId_t BlinkTurnMutexHandle;
+const osMutexAttr_t BlinkTurnMutex_attributes = {
+  .name = "BlinkTurnMutex"
+};
+/* USER CODE BEGIN PV */
+
+extern float humidity = 60.0f; // updated by your I2C task
+#define PWM_1V   19800
+#define PWM_2V   39700
+#define HUMIDITY_THRESHOLD 40.0f  // choose your switch point
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM4_Init(void);
+static void MX_I2C1_Init(void);
+void StartBlinkGreenTask(void *argument);
+void StartBlinkRedTask(void *argument);
+void StartSetPWM(void *argument);
+void StartReadHumidity(void *argument);
+void StartReadTouchLCD(void *argument);
+void StartSetLCDState(void *argument);
+void CallbackSleepTimer(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM4_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+  /* Create the mutex(es) */
+  /* creation of BlinkTurnMutex */
+  BlinkTurnMutexHandle = osMutexNew(&BlinkTurnMutex_attributes);
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* Create the timer(s) */
+  /* creation of sleepTimer */
+  sleepTimerHandle = osTimerNew(CallbackSleepTimer, osTimerOnce, NULL, &sleepTimer_attributes);
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of blinkGreenTask */
+  blinkGreenTaskHandle = osThreadNew(StartBlinkGreenTask, NULL, &blinkGreenTask_attributes);
+
+  /* creation of blinkRedTask */
+  blinkRedTaskHandle = osThreadNew(StartBlinkRedTask, NULL, &blinkRedTask_attributes);
+
+  /* creation of setPWM */
+  setPWMHandle = osThreadNew(StartSetPWM, NULL, &setPWM_attributes);
+
+  /* creation of readHumidity */
+  readHumidityHandle = osThreadNew(StartReadHumidity, NULL, &readHumidity_attributes);
+
+  /* creation of readTouchLCD */
+  readTouchLCDHandle = osThreadNew(StartReadTouchLCD, NULL, &readTouchLCD_attributes);
+
+  /* creation of setLCDState */
+  setLCDStateHandle = osThreadNew(StartSetLCDState, NULL, &setLCDState_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 0;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 65535;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+  HAL_TIM_MspPostInit(&htim4);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PG13 PG14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartBlinkGreenTask */
+/**
+  * @brief  Function implementing the blinkGreenTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartBlinkGreenTask */
+void StartBlinkGreenTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+	uint8_t countGreen = 0;
+  /* Infinite loop */
+  for(;;)
+  {
+	  if (osMutexAcquire(BlinkTurnMutexHandle, osWaitForever) == osOK)
+	  {
+		  while(countGreen < 5 )
+		  {
+			  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13,GPIO_PIN_SET);
+			  osDelay(500);
+			  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13,GPIO_PIN_RESET);
+			  osDelay(500);
+			  countGreen++;
+		  }
+		  countGreen = 0;
+		  osMutexRelease(BlinkTurnMutexHandle);
+
+		  osDelay(100);
+	  }
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_StartBlinkRedTask */
+/**
+* @brief Function implementing the blinkRedTask thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartBlinkRedTask */
+void StartBlinkRedTask(void *argument)
+{
+  /* USER CODE BEGIN StartBlinkRedTask */
+	uint8_t countRed = 0;
+  /* Infinite loop */
+  for(;;)
+  {
+	  if (osMutexAcquire(BlinkTurnMutexHandle, osWaitForever) == osOK)
+	  {
+		  while(countRed < 10 )
+		  {
+			  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_14,GPIO_PIN_SET);
+			  osDelay(250);
+			  HAL_GPIO_WritePin(GPIOG, GPIO_PIN_14,GPIO_PIN_RESET);
+			  osDelay(250);
+			  countRed++;
+		  }
+		  countRed = 0;
+		  osMutexRelease(BlinkTurnMutexHandle);
+
+		  osDelay(100);
+	  }
+  }
+  /* USER CODE END StartBlinkRedTask */
+}
+
+/* USER CODE BEGIN Header_StartSetPWM */
+/**
+* @brief Function implementing the setPWM thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartSetPWM */
+void StartSetPWM(void *argument)
+{
+  /* USER CODE BEGIN StartSetPWM */
+  /* Infinite loop */
+	// Start PWM on TIM4 CH1
+	    HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+
+	    for(;;)
+	    {
+	        uint16_t duty;
+
+	        if (humidity < HUMIDITY_THRESHOLD)
+	        {
+	            duty = PWM_1V;    // output ≈ 1V
+	        }
+	        else
+	        {
+	            duty = PWM_2V;    // output ≈ 2V
+	        }
+
+	        TIM4->CCR1 = duty;
+	        //TIM4->CCR1 = 65535;
+
+
+	        osDelay(200); // update 5 times per second
+	    }
+  /* USER CODE END StartSetPWM */
+}
+
+/* USER CODE BEGIN Header_StartReadHumidity */
+/**
+* @brief Function implementing the readHumidity thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartReadHumidity */
+void StartReadHumidity(void *argument)
+{
+  /* USER CODE BEGIN StartReadHumidity */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartReadHumidity */
+}
+
+/* USER CODE BEGIN Header_StartReadTouchLCD */
+/**
+* @brief Function implementing the readTouchLCD thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartReadTouchLCD */
+void StartReadTouchLCD(void *argument)
+{
+  /* USER CODE BEGIN StartReadTouchLCD */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartReadTouchLCD */
+}
+
+/* USER CODE BEGIN Header_StartSetLCDState */
+/**
+* @brief Function implementing the setLCDState thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartSetLCDState */
+void StartSetLCDState(void *argument)
+{
+  /* USER CODE BEGIN StartSetLCDState */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartSetLCDState */
+}
+
+/* CallbackSleepTimer function */
+void CallbackSleepTimer(void *argument)
+{
+  /* USER CODE BEGIN CallbackSleepTimer */
+
+  /* USER CODE END CallbackSleepTimer */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM2 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM2)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
