501b238f2569871aaf0cf4f61e1a31beefdedb36
Alexis <alexis.cote.3@ulaval.ca>
2025-12-04 14:36:29 -0500

    changement gitignore

--- Projet_SMI_Test/Core/Src/stm32f4xx_hal_msp.c
+++ Projet_SMI_Test/Core/Src/stm32f4xx_hal_msp.c
@@ -192,8 +192,8 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
     */
     GPIO_InitStruct.Pin = GPIO_PIN_12;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
